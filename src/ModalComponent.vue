c7783a84360c785aec4c608db697c0e66281b4bc
남우현 <skyainq@naver.com>
2023-04-23 15:00:56 +0900

    watcher로 input데이터 골라잡기

--- src/ModalComponent.vue
+++ src/ModalComponent.vue
@@ -4,7 +4,7 @@
       <h4>{{onerooms[modalPressed].title}}</h4>
       <img :src="onerooms[modalPressed].image" style = "width: 100%">
       <p>{{onerooms[modalPressed].content}}</p>
-      <input v-model.number = "month">
+      <input v-model = "month">
       <p>{{month}}개월 선택 시: {{onerooms[modalPressed].price * month}}원</p>
       <button @click = "$emit('closeModal')">닫기</button>
     </div>
@@ -19,6 +19,28 @@ export default {
         month : 1,
       }
     },
+    watch : {
+      month(a){
+        if(a >= 13){
+          this.month = 1
+          alert("12개월까지 구매 가능합니다.")
+        } else if(isNaN(a)){
+          this.month = 1
+          alert("숫자를 입력해주십시오.")
+        }
+      }
+    },
+    // watch : {
+    //   month(a){
+    //     if(a >= 13){
+    //       alert("12개월까지 구매 가능합니다.")
+    //       this.month = 1
+    //     } else if(a.isNaN()){
+    //       alert("숫자를 입력해주십시오.")
+    //       this.month = 1
+    //     } 
+    //   }
+    // },
     props : {
         onerooms : Array,
         modalPressed : Number,
